ebe980d039108b443f7a6fa2b0042e72a4a9e179
Pasquale PL. Labate <pasquale.labate@senseisrl.local>
2019-06-19 16:45:55 +0200

    test 04 with testClass

---
+++ src/test/java/devOps/devOps/TestMessageBuilder.java
@@ -0,0 +1,15 @@
+package devOps.devOps;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestMessageBuilder {
+
+	@Test
+    public void testName() {
+		App gm = new App(); 
+		assertEquals("Hello Pasq",  gm.getMessage("pas"));
+	}
+	
+}
