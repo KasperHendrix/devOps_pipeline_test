c5f3b7a2adb11bc0074df71e6e37ce7f74fe373d
Pasquale PL. Labate <pasquale.labate@senseisrl.local>
2019-06-26 14:30:29 +0200

    test 24

--- src/test/java/devOps/devOps/TestMessageBuilder.java
+++ src/test/java/devOps/devOps/TestMessageBuilder.java
@@ -18,11 +18,11 @@ public class TestMessageBuilder {
 		assertEquals("Please provide a name!", app1.getMessage(" "));
 	}
 	
-//	@Test
-//	public void testNameNull() {
-//		App app2 = new App();
-//		assertEquals("Please provide a name!", app2.getMessage(null));
-//	}
+	@Test
+	public void testNameNull() {
+		App app2 = new App();
+		assertEquals("Please provide a name!", app2.getMessage(null));
+	}
 	
 //	@Test
 //	public void testNameWithNumbers() {
