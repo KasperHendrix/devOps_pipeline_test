b7ca3a1cda7356c966c4bc7508c24e90de91a9bc
Pasquale PL. Labate <pasquale.labate@senseisrl.local>
2019-06-27 09:57:46 +0200

    test 30

--- src/main/java/devOps/devOps/App.java
+++ src/main/java/devOps/devOps/App.java
@@ -6,7 +6,7 @@ package devOps.devOps;
  */
 public class App {
 	public static void main(String[] args) {
-		String msg = "Pasq";
+		String msg = "Pas";
 		getMessage(msg);
 	}
 
--- src/test/java/devOps/devOps/TestMessageBuilder.java
+++ src/test/java/devOps/devOps/TestMessageBuilder.java
@@ -7,28 +7,21 @@ import org.junit.Test;
 public class TestMessageBuilder {
 
 	@Test
-    public void testName() {
-		App app = new App(); 
-		assertEquals("Hello Pasq",  app.getMessage("Pasq"));
+	public void testName() {
+		App app = new App();
+		assertEquals("Hello Pas", app.getMessage("Pas"));
 	}
-	
+
 	@Test
 	public void testNameEmpty() {
 		App app1 = new App();
 		assertEquals("Please provide a name!", app1.getMessage(" "));
 	}
-	
+
 	@Test
 	public void testNameNull() {
 		App app2 = new App();
 		assertEquals("Please provide a name!", app2.getMessage(null));
 	}
-	
-//	@Test
-//	public void testNameWithNumbers() {
-//		App app2 = new App();
-//		assertEquals("Please provide a name!", app2.getMessage("p4sq"));
-//	}
-	
-	
+
 }
